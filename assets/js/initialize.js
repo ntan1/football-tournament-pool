e48c08919bcde9e21b372c0527389c7de1cb6149
Nick Tan <nctan1002@gmail.com>
2018-06-05 14:08:34 -0400

    Rough auth

---
+++ assets/js/initialize.js
@@ -0,0 +1,14 @@
+// Initialize Firebase
+const config = {
+    apiKey: "AIzaSyCJ2QBEtwDJ12gwLI3kKPuCPYnvQdFTatc",
+    authDomain: "football-tournament-pool.firebaseapp.com",
+    databaseURL: "https://football-tournament-pool.firebaseio.com",
+    projectId: "football-tournament-pool",
+    storageBucket: "football-tournament-pool.appspot.com",
+    messagingSenderId: "528387678821"
+};
+firebase.initializeApp(config);
+db = firebase.database();
+
+// firebase auth
+const provider = new firebase.auth.GoogleAuthProvider();
