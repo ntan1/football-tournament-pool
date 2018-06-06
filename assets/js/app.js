d946e44e6d103d4020bce9530176847d07bd5ae3
ntan1 <nctan1002@gmail.com>
2018-06-06 19:38:08 -0400

    Same page auth

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,56 @@
 $(document).ready(function () {
-    firebase.auth().signInWithRedirect(provider);
-    firebase.auth().getRedirectResult().then(function(result) {
-        if (result.credential) {
-          // This gives you a Google Access Token. You can use it to access the Google API.
-          var token = result.credential.accessToken;
-          // ...
-        }
-        // The signed-in user info.
-        var user = result.user;
-        console.log(user)
-      }).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
-      });
+  // Sign in with reidrect 
+  // firebase.auth().signInWithRedirect(provider);
+  // firebase.auth().getRedirectResult().then(function (result) {
+  //   if (result.credential) {
+  //     // This gives you a Google Access Token. You can use it to access the Google API.
+  //     let token = result.credential.accessToken;
+  //     // ...
+  //   }
+  //   // The signed-in user info.
+  //   let user = result.user;
+  //   console.log(user)
+  // }).catch(function (error) {
+  //   // Handle Errors here.
+  //   let errorCode = error.code;
+  //   let errorMessage = error.message;
+  //   // The email of the user's account used.
+  //   let email = error.email;
+  //   // The firebase.auth.AuthCredential type that was used.
+  //   let credential = error.credential;
+  //   // ...
+  // });
+
+  var uiConfig = {
+    callbacks: {
+      signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+        // User successfully signed in.
+        // Return type determines whether we continue the redirect automatically
+        // or whether we leave that to developer to handle.
+        return true;
+      },
+      uiShown: function () {
+        // The widget is rendered.
+        // Hide the loader.
+        document.getElementById('loader').style.display = 'none';
+      }
+    },
+    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+    signInFlow: 'popup',
+    signInSuccessUrl: '<url-to-redirect-to-on-success>',
+    signInOptions: [
+      // Leave the lines as is for the providers you want to offer your users.
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      firebase.auth.PhoneAuthProvider.PROVIDER_ID
+    ],
+    // Terms of service url.
+    tosUrl: '<your-tos-url>'
+  };
+
+  // The start method will wait until the DOM is loaded.
+  ui.start('#firebaseui-auth-container', uiConfig);
 });
