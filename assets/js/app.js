4731ba3bd6cad042d6e3b9ca926b482af75886fb
ntan1 <nctan1002@gmail.com>
2018-06-06 19:48:15 -0400

    Added redirect page

--- assets/js/app.js
+++ assets/js/app.js
@@ -40,15 +40,10 @@ $(document).ready(function () {
     signInSuccessUrl: '<url-to-redirect-to-on-success>',
     signInOptions: [
       // Leave the lines as is for the providers you want to offer your users.
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-      firebase.auth.GithubAuthProvider.PROVIDER_ID,
-      firebase.auth.EmailAuthProvider.PROVIDER_ID,
-      firebase.auth.PhoneAuthProvider.PROVIDER_ID
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID
     ],
     // Terms of service url.
-    tosUrl: '<your-tos-url>'
+    tosUrl: 'home.html'
   };
 
   // The start method will wait until the DOM is loaded.
