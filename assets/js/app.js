e5a875cb7ede2f09fbe8dc8383bbb775e15d8d66
ntan1 <nctan1002@gmail.com>
2018-06-06 19:50:51 -0400

    Correctly added redirect url

--- assets/js/app.js
+++ assets/js/app.js
@@ -37,13 +37,13 @@ $(document).ready(function () {
     },
     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
     signInFlow: 'popup',
-    signInSuccessUrl: '<url-to-redirect-to-on-success>',
+    signInSuccessUrl: 'home.html',
     signInOptions: [
       // Leave the lines as is for the providers you want to offer your users.
       firebase.auth.GoogleAuthProvider.PROVIDER_ID
     ],
     // Terms of service url.
-    tosUrl: 'home.html'
+    tosUrl: '<your-tos-url>'
   };
 
   // The start method will wait until the DOM is loaded.
