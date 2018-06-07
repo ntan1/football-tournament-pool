db7069fd5ef11769c55f5e9297d0246a042622b4
Nick Tan <nctan1002@gmail.com>
2018-06-07 11:11:15 -0400

    test user data

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
   let redirectUrl = "home.html";
-  let user;
+  let user = firebase.auth().currentUser;
 
   // firebase.auth().onAuthStateChanged(function (user) {
   //   // user = user;
@@ -11,13 +11,26 @@ $(document).ready(function () {
   //     $("#welcome-msg").text(`Welcome ${user.displayName}`);
   //   } else {
   //     window.location = 'index.html';
-  
+
   //   }
   // });
 
+  if (user != null) {
+    user.providerData.forEach(function (profile) {
+      console.log("Sign-in provider: " + profile.providerId);
+      console.log("  Provider-specific UID: " + profile.uid);
+      console.log("  Name: " + profile.displayName);
+      console.log("  Email: " + profile.email);
+      console.log("  Photo URL: " + profile.photoURL);
+    });
+    console.log("user obj" + user);
+  }
+  $("#welcome-msg").text(`Welcome ${user.displayName}`);
 
-  db.ref("/").on("value", function(snap) {
+  db.ref("/").on("value", function (snap) {
     console.log(snap.val());
   });
+
+  db.ref("/")
 
 });
