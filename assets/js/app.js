8d27764a14dae065307e559d1f15ca78bca7fed7
Nick Tan <nctan1002@gmail.com>
2018-06-07 01:40:23 -0400

    Test auth read 3

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,17 +4,17 @@ $(document).ready(function () {
   let user;
 
   firebase.auth().onAuthStateChanged(function (user) {
-    user = user;
+    // user = user;
+    if (user) {
+      // User is signed in.
+      console.log(user);
+      $("#welcome-msg").text(`Welcome ${user.displayName}`);
+    } else {
+      window.location = 'index.html';
+  
+    }
   });
 
-  if (user) {
-    // User is signed in.
-    console.log(user);
-    $("#welcome-msg").text(`Welcome ${user.displayName}`);
-  } else {
-    window.location = 'index.html';
-
-  }
 
   db.ref("").on("value", function(snap) {
     console.log(snap.value);
