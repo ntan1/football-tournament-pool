b84d0a931a4d6cbeca7349f7e7be7d76cc088621
Nick Tan <nctan1002@gmail.com>
2018-06-07 17:43:42 -0400

    Basic functions for adding group stage matches, teams, stadiums, to firestore

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
   let redirectUrl = "home.html";
-  let user = firebase.auth().currentUser;
+  // let user = firebase.auth().currentUser;
 
   // firebase.auth().onAuthStateChanged(function (user) {
   //   // user = user;
@@ -15,22 +15,63 @@ $(document).ready(function () {
   //   }
   // });
 
-  if (user != null) {
-    user.providerData.forEach(function (profile) {
-      console.log("Sign-in provider: " + profile.providerId);
-      console.log("  Provider-specific UID: " + profile.uid);
-      console.log("  Name: " + profile.displayName);
-      console.log("  Email: " + profile.email);
-      console.log("  Photo URL: " + profile.photoURL);
-    });
-    console.log("user obj" + user);
+  // if (user != null) {
+  //   user.providerData.forEach(function (profile) {
+  //     console.log("Sign-in provider: " + profile.providerId);
+  //     console.log("  Provider-specific UID: " + profile.uid);
+  //     console.log("  Name: " + profile.displayName);
+  //     console.log("  Email: " + profile.email);
+  //     console.log("  Photo URL: " + profile.photoURL);
+  //   });
+  //   console.log("user obj" + user);
+  // }
+  // $("#welcome-msg").text(`Welcome ${user.displayName}`);
+
+  // fb.ref("/").on("value", function (snap) {
+  //   console.log(snap.val());
+  // });
+
+  // add matches to database
+
+  const tournament = "world-cup-2018";
+  const wcRef = db.collection(tournament);
+
+  function addMatches(tournament, stage) {
+    // $.each(matches[stage], function (group, val) {
+    //   wcRef.doc("matches").collection("groups").doc(group).set(val)
+      wcRef.doc("matches").set(matches.groups)
+        .then(function () {
+          console.log("written");
+        })
+        .catch(function (error) {
+          console.log("Error adding doc: " + error)
+        });
+    // });
   }
-  $("#welcome-msg").text(`Welcome ${user.displayName}`);
 
-  db.ref("/").on("value", function (snap) {
-    console.log(snap.val());
-  });
+  function addStadiums(tournament) {
+    wcRef.doc("stadiums").set(stadiums)
+      .then(function () {
+        console.log("written");
+      })
+      .catch(function (error) {
+        console.log("Error adding doc: " + error)
+      });
+  }
 
-  db.ref("/")
+  function addTeams(tournament) {
+    wcRef.doc("teams").set(teams)
+      .then(function () {
+        console.log("written");
+      })
+      .catch(function (error) {
+        console.log("Error adding doc: " + error)
+      });
+  }
+
+  addMatches(tournament, "groups");
+  // addStadiums(tournament);
+  // addTeams(tournament);
+
 
 });
