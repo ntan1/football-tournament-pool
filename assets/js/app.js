3a78cce2425fe2ed8523fd27f579a72610e565ac
Nick Tan <nctan1002@gmail.com>
2018-06-07 01:29:35 -0400

    Test auth read

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,66 +1,22 @@
 $(document).ready(function () {
-  // Sign in with reidrect 
-  // firebase.auth().signInWithRedirect(provider);
-  // firebase.auth().getRedirectResult().then(function (result) {
-  //   if (result.credential) {
-  //     // This gives you a Google Access Token. You can use it to access the Google API.
-  //     let token = result.credential.accessToken;
-  //     // ...
-  //   }
-  //   // The signed-in user info.
-  //   let user = result.user;
-  //   console.log(user)
-  // }).catch(function (error) {
-  //   // Handle Errors here.
-  //   let errorCode = error.code;
-  //   let errorMessage = error.message;
-  //   // The email of the user's account used.
-  //   let email = error.email;
-  //   // The firebase.auth.AuthCredential type that was used.
-  //   let credential = error.credential;
-  //   // ...
-  // });
 
   let redirectUrl = "home.html";
 
   firebase.auth().onAuthStateChanged(function (user) {
-    if (user) {
-      // User is signed in.
-      console.log(user);
-      window.location = 'home.html';
-      $("#welcome-msg").text(`Welcome ${user.displayName}`);
-    } else {
-      let uiConfig = {
-        callbacks: {
-          signInSuccessWithAuthResult: function (authResult, redirectUrl) {
-            // User successfully signed in.
-            // Return type determines whether we continue the redirect automatically
-            // or whether we leave that to developer to handle.
-            console.log(authResult);
-            return true;
-          },
-          uiShown: function () {
-            // The widget is rendered.
-            // Hide the loader.
-            document.getElementById('loader').style.display = 'none';
-          }
-        },
-        // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-        signInFlow: 'popup',
-        signInSuccessUrl: redirectUrl,
-        signInOptions: [
-          // Leave the lines as is for the providers you want to offer your users.
-          firebase.auth.GoogleAuthProvider.PROVIDER_ID
-        ],
-        // Terms of service url.
-        // tosUrl: '<your-tos-url>'
-      };
-      
+    let user = user;
+  });
 
-      // The start method will wait until the DOM is loaded.
-      ui.start('#firebaseui-auth-container', uiConfig);
+  if (user) {
+    // User is signed in.
+    console.log(user);
+    $("#welcome-msg").text(`Welcome ${user.displayName}`);
+  } else {
+    window.location = 'index.html';
 
-    }
+  }
+
+  db.ref("").on("value", function(snap) {
+    console.log(snap.value);
   });
 
 });
---
+++ assets/js/login.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+    // Sign in with reidrect 
+    // firebase.auth().signInWithRedirect(provider);
+    // firebase.auth().getRedirectResult().then(function (result) {
+    //   if (result.credential) {
+    //     // This gives you a Google Access Token. You can use it to access the Google API.
+    //     let token = result.credential.accessToken;
+    //     // ...
+    //   }
+    //   // The signed-in user info.
+    //   let user = result.user;
+    //   console.log(user)
+    // }).catch(function (error) {
+    //   // Handle Errors here.
+    //   let errorCode = error.code;
+    //   let errorMessage = error.message;
+    //   // The email of the user's account used.
+    //   let email = error.email;
+    //   // The firebase.auth.AuthCredential type that was used.
+    //   let credential = error.credential;
+    //   // ...
+    // });
+
+    let redirectUrl = "home.html";
+
+    let uiConfig = {
+        callbacks: {
+            signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+                // User successfully signed in.
+                // Return type determines whether we continue the redirect automatically
+                // or whether we leave that to developer to handle.
+                console.log(authResult);
+                return true;
+            },
+            uiShown: function () {
+                // The widget is rendered.
+                // Hide the loader.
+                document.getElementById('loader').style.display = 'none';
+            }
+        },
+        // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+        signInFlow: 'popup',
+        signInSuccessUrl: redirectUrl,
+        signInOptions: [
+            // Leave the lines as is for the providers you want to offer your users.
+            firebase.auth.GoogleAuthProvider.PROVIDER_ID
+        ],
+        // Terms of service url.
+        // tosUrl: '<your-tos-url>'
+    };
+
+
+    // The start method will wait until the DOM is loaded.
+    ui.start('#firebaseui-auth-container', uiConfig);
+
+});
