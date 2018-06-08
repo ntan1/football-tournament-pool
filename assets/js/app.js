4ff8ca79b1e6afe8129196229abee6005c3231ae
Nick Tan <nctan1002@gmail.com>
2018-06-08 00:09:47 -0400

    app test

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,14 +16,14 @@ $(document).ready(function () {
   // });
 
   if (user != null) {
-    user.providerData.forEach(function (profile) {
-      console.log("Sign-in provider: " + profile.providerId);
-      console.log("  Provider-specific UID: " + profile.uid);
-      console.log("  Name: " + profile.displayName);
-      console.log("  Email: " + profile.email);
-      console.log("  Photo URL: " + profile.photoURL);
-    });
     console.log("user obj" + user);
+    user.providerData.forEach(function (profile) {
+      // console.log("Sign-in provider: " + profile.providerId);
+      // console.log("  Provider-specific UID: " + profile.uid);
+      // console.log("  Name: " + profile.displayName);
+      // console.log("  Email: " + profile.email);
+      // console.log("  Photo URL: " + profile.photoURL);
+    });
   }
   $("#welcome-msg").text(`Welcome ${user.displayName}`);
 
