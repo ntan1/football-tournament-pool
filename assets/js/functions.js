7de18993b2a8acdbf1611730aa962bc333dd951a
Nick Tan <nctan1002@gmail.com>
2018-06-08 03:20:05 -0400

    get predictions test

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,10 +1,12 @@
 function addFixture(match) {
     let matchDiv = $(`<div class='col-8 match' data-id=${match.id}>`);
-    let dateDiv = $("<div class='col-4'>")
+    let dateDiv = $("<div class='col-4'>");
+    let homePredict = $(`<input type="text" size="2" class="home-result">`).val(predictions[match.id].home_result);
+    let awayPredict = $(`<input class="away-result" type="text" size="2">`).val(predictions[match.id].away_result);
     dateDiv.append(`Match starts <span class="match-start-in">${moment(match.date).from(moment())}</span> at `);
     dateDiv.append(`<span class="match-time">${moment(match.date).format("M/D h:mma")}</span>`);
-    matchDiv.append(`<label class="home-team">${getTeamName(match.home_team)}</label><input type="text" size="2" class="home-result"> vs `);
-    matchDiv.append(`<input class="away-result" type="text" size="2"><label class="away-team">${getTeamName(match.away_team)}</label>`);
+    matchDiv.append(`<label class="home-team">${getTeamName(match.home_team)}</label>${homePredict} vs `);
+    matchDiv.append(`${awayPredict}<label class="away-team">${getTeamName(match.away_team)}</label>`);
     $("#predictions").append(matchDiv, dateDiv);
 }
 
@@ -18,16 +20,26 @@ function uploadPredictions() {
     usersRef.doc(user.providerData[0].uid).get()
         .then(function (snap) {
             if (snap.exists) {
-                usersRef.doc(user.providerData[0].uid).update({predictions: Object.assign({}, predictions)});
+                usersRef.doc(user.providerData[0].uid).update({ predictions: Object.assign({}, predictions) });
                 console.log("updated uid");
             } else {
-                usersRef.doc(user.providerData[0].uid).set({name: user.displayName});
-                usersRef.doc(user.providerData[0].uid).set({predictions: Object.assign({}, predictions)});
+                usersRef.doc(user.providerData[0].uid).set({ name: user.displayName });
+                usersRef.doc(user.providerData[0].uid).set({ predictions: Object.assign({}, predictions) });
                 console.log("set new uid");
             }
         })
 }
 
 function getPredictions(uid) {
-    console.log(usersRef.doc(uid).data);
+    usersRef.doc(uid).get()
+        .then(function (snap) {
+            if (snap.exists) {
+                $.forEach(snap.data()["predictions"], function (id, val) {
+                    predictions[id] = val;
+                });
+            } else {
+
+            }
+        });
+    console.log(usersRef.doc(uid).data());
 }
