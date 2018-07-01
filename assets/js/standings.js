2774bdfd31c1a239c83f9c2d29db836c680e0311
Nick Tan <nctan1002@gmail.com>
2018-07-01 01:18:56 -0400

    Made standings more efficient

--- assets/js/standings.js
+++ assets/js/standings.js
@@ -5,52 +5,33 @@ $(document).ready(function () {
         if (theUser) {
             // User is signed in.
             user = theUser;
-            // get all user predictions
-
+            // get standings
+            wcRef.doc("standings").get()
+                .then(doc => {
+                    console.log(doc.data());
+                    $.each(doc.data(), (user, data) => {
+                        console.log(data.name);
+                        standings.push({
+                            name: data.name,
+                            points: data.points,
+                        });
+                    });
+                    console.log(standings);
+                    standings.sort((a, b) => {
+                        return b.points - a.points;
+                    })
+                    for (let i = 0; i < standings.length; i++) {
+                        $("#standings-table").find("tbody").append(
+                            `<tr><td>${i + 1}</td><td>${standings[i].name}</td><td>${standings[i].points}</td></tr>`
+                        );
+                    }
+                });
         } else {
             // No user is signed in.
             console.log("not logged in");
         }
     });
 
-    usersRef.get()
-        .then(docs => {
-            getMatches(() => {
-                let pos = 1;
-                docs.forEach(user => {
-                    // console.log(user.id + ": " + user.data().name);
-                    let userPredictions = [];
-                    let userPts = 0;
-                    // userPredictions = getPredictionsTest(user.id, userPredictions);
-                    getPredictionsTest(user.id, userPredictions, (pred) => {
-                        // console.log(matches);
-                        for (let i = 1; i < matches.length; i++) {
-                            if (matches[i]["home_result"] !== null) {
-                                if (pred[i]) {
-                                    userPts += calcPts(matches[i], pred[i], i);
-                                }
-                            }
-                        }
-                        standings.push({
-                            name: user.data().name,
-                            pts: userPts
-                        });
-                        $("#standings-table").find("tbody").append(
-                            `<tr><td>${pos}</td><td>${user.data().name}</td><td>${userPts}</td></tr>`
-                        );
-                        pos++;
-                    });
-                });
-            });
-            // standings.sort((a,b) => {
-            //     return a.pts - b.pts;
-            // })
-            // for (let i = 0; i < standings.length; i++) {
-            //     $("#standings-table").find("tbody").append(
-            //         `<tr><td>${i+1}</td><td>${standings[i].name}</td><td>${standings[i].pts}</td></tr>`
-            //     );
-            // }
-        });
 
     // create functions for getting pts and rendering table
     // create new standings page to use above functions
